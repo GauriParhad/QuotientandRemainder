7f9c42ca41f4873ee97a0e5658f642eb0e6d2a05
Gaurip10 <thegauriparhad@gmail.com>
2021-08-08 16:47:38 +0530

    QuotientandRemainder

---
+++ src/QuotientandRemainder.java
@@ -0,0 +1,11 @@
+public class QuotientandRemainder {
+    public static void main(String[] args) {
+
+        int Dividend = 50, Divisor = 4;
+        int Remainder = Dividend % Divisor;
+        int Quotient = Dividend / Divisor;
+
+        System.out.println("Remainder = " +Remainder);
+        System.out.println("Quotient = " +Quotient);
+    }
+}
